d60910f298c9ae1a1eaa4637c4fb9d54a1a523de
officialBusiness <a634318935@126.com>
2024-09-29 20:33:15 +0800

    lifeRestart:完善 lifeRestart 到 e38daf3ebbf9ade07c38fd9496ad6800e9b588f2

--- github_repositories/lifeRestart/reproduction/src/index.js
+++ github_repositories/lifeRestart/reproduction/src/index.js
@@ -1,5 +1,23 @@
 import App from './app.js';
 
+globalThis.$$eventMap = new Map();
+globalThis.$$event = (tag, data) => {
+	const listener = $$eventMap.get(tag);
+	if (listener) listener.forEach(fn => fn(data));
+}
+globalThis.$$on = (tag, fn) => {
+	let listener = $$eventMap.get(tag);
+	if (!listener) {
+		listener = new Set();
+		$$eventMap.set(tag, listener);
+	}
+	listener.add(fn);
+}
+globalThis.$$off = (tag, fn) => {
+	const listener = $$eventMap.get(tag);
+	if (listener) listener.delete(fn);
+}
+
 globalThis.json = async fileName => await (await fetch(`../data/${fileName}.json`)).json();
 
 // Pssst, I've created a github package - https://github.com/brookesb91/dismissible
@@ -11,5 +29,4 @@ globalThis.hideBanners = (e) => {
 
 const app = new App();
 app.initial();
-
 globalThis.app = app;
